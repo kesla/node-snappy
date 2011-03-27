a8b2b032efea2a85fa7bcd759b1a8de0deeae01d
David Björklund <david.bjorklund@gmail.com>
2011-03-27 13:31:26 +0200

    typo

--- example.js
+++ example.js
@@ -2,13 +2,13 @@
 
 snappy = require('./build/default/snappy')
 
-comp = binding.compress("foo foo foo  Fasfa daos asd foo foo foo asdasf bar bar aarr")
+comp = snappy.compress("foo foo foo  Fasfa daos asd foo foo foo asdasf bar bar aarr")
 base64 = comp.toString('base64')
 comp2 = new Buffer(base64, 'base64')
-console.log(binding.uncompress(comp2))
+console.log(snappy.uncompress(comp2))
 console.log(
-	binding.uncompress(
-		binding.compress(
+	snappy.uncompress(
+		snappy.compress(
 			"foobar foobar foobar foobar foobar foobar foobar foobar foobar"
 		)
 	)
