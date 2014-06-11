a5a08c147dc1da74044093ca7e6741b16da1a6ca
David Björklund <david.bjorklund@gmail.com>
2014-06-11 12:20:34 -0400

    test uncompressing to a buffer

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var spawn = require('child_process').spawn
 
   , test = require('tap').test
 
-test('uncompress', function (t) {
+test('uncompress small string', function (t) {
   var child = spawn('python', [ '-m', 'snappy', '-c' ])
     , uncompressStream = require('./stream').createUncompressStream({ asBuffer: false })
     , data = ''
@@ -22,5 +22,26 @@ test('uncompress', function (t) {
   child.stdin.write('beep')
   child.stdin.write(' ')
   child.stdin.write('boop')
+  child.stdin.end()
+})
+
+test('uncompress small Buffer', function (t) {
+  var child = spawn('python', [ '-m', 'snappy', '-c' ])
+    , uncompressStream = require('./stream').createUncompressStream({ asBuffer: true })
+    , data = []
+
+  uncompressStream.on('data', function (chunk) {
+    data.push(chunk)
+    t.ok(Buffer.isBuffer(chunk))
+  })
+
+  uncompressStream.on('end', function () {
+    t.deepEqual(Buffer.concat(data), new Buffer('beep boop'))
+    t.end()
+  })
+
+  child.stdout.pipe(uncompressStream)
+
+  child.stdin.write(new Buffer('beep boop'))
   child.stdin.end()
 })
