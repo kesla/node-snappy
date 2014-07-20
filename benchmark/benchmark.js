2a27f449d946f90797f87460ca2ff06fe492b909
David Björklund <david.bjorklund@gmail.com>
2014-07-20 17:45:32 +0200

    benchmark against optimised zlib.Gzip

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -10,6 +10,28 @@ var zlib = require('zlib')
   , round = function (number) {
       return Math.round(number * 100) / 100
     }
+  , customGzip = function (callback) {
+      var buffers = []
+        , size = 0
+        , gzip = new zlib.Gzip({
+              level: zlib.Z_BEST_SPEED
+            , memLevel: zlib.Z_MAX_MEMLEVEL
+          })
+
+      gzip.on('data', function (buffer) {
+        buffers.push(buffer)
+        size += buffer.length
+      })
+
+      gzip.on('end', function () {
+        callback(null, Buffer.concat(buffers, size))
+        buffers.length = 0
+      })
+
+      gzip.write(input)
+
+      gzip.end()
+    }
 
 console.log('input size', bytes(input.length))
 require('run-series')([
@@ -37,6 +59,23 @@ require('run-series')([
         , function (err, event) {
             console.log(event.target.toString())
             zlib.gzip(input, function (err, compressed) {
+              console.log(
+                  'compressed size'
+                , bytes(compressed.length)
+                , '(' + round(compressed.length / input.length * 100) + '%)'
+              )
+              done()
+            })
+          }
+      )
+    }
+  , function (done) {
+      benchmark(
+          'zlib.Gzip with custom options'
+        , customGzip
+        , function (err, event) {
+            console.log(event.target.toString())
+            customGzip(function (err, compressed) {
               console.log(
                   'compressed size'
                 , bytes(compressed.length)
