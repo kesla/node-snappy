431fcb2d84d20b92bfb5708717f2a768b62a5543
David Björklund <david.bjorklund@gmail.com>
2016-04-07 08:59:39 +0200

    update to ava@0.14

--- test.js
+++ test.js
@@ -13,12 +13,12 @@ const {compressSync, isValidCompressedSync, uncompressSync} = snappy;
 
 test('compress() string', function * (t) {
   const buffer = yield compress(inputString);
-  t.ok(Buffer.isBuffer(buffer), 'should return a Buffer');
+  t.truthy(Buffer.isBuffer(buffer), 'should return a Buffer');
 });
 
 test('compress() buffer', function * (t) {
   const buffer = yield compress(inputBuffer);
-  t.ok(Buffer.isBuffer(buffer), 'should return a Buffer');
+  t.truthy(Buffer.isBuffer(buffer), 'should return a Buffer');
 });
 
 test('compress() bad input', function * (t) {
@@ -27,59 +27,59 @@ test('compress() bad input', function * (t) {
 
 test('compressSync() string', function * (t) {
   const buffer = compressSync(inputString);
-  t.ok(Buffer.isBuffer(buffer), 'should return a Buffer');
+  t.truthy(Buffer.isBuffer(buffer), 'should return a Buffer');
 });
 
 test('compressSync() buffer', function * (t) {
   const buffer = compressSync(inputBuffer);
-  t.ok(Buffer.isBuffer(buffer), 'should return a Buffer');
+  t.truthy(Buffer.isBuffer(buffer), 'should return a Buffer');
 });
 
 test('isValidCompressed() on valid data', function * (t) {
   const compressed = yield compress(inputBuffer);
   const isCompressed = yield isValidCompressed(compressed);
-  t.ok(isCompressed);
+  t.truthy(isCompressed);
 });
 
 test('isValidCompressed() on invalid data', function * (t) {
   const isCompressed = yield isValidCompressed(new Buffer('beep boop'));
-  t.notOk(isCompressed);
+  t.falsy(isCompressed);
 });
 
 test('isValidCompressedSync() on valid data', function * (t) {
   const compressed = yield compress(inputBuffer);
   const isCompressed = isValidCompressedSync(compressed);
-  t.ok(isCompressed);
+  t.truthy(isCompressed);
 });
 
 test('isValidCompressedSync() on invalid data', function * (t) {
   const isCompressed = isValidCompressedSync(new Buffer('beep boop'));
-  t.notOk(isCompressed);
+  t.falsy(isCompressed);
 });
 
 test('uncompress() defaults to Buffer', function * (t) {
   const compressed = yield compress(inputBuffer);
   const buffer = yield uncompress(compressed);
-  t.same(buffer, inputBuffer);
+  t.deepEqual(buffer, inputBuffer);
 });
 
 test('uncompress() returning a Buffer', function * (t) {
   const compressed = yield compress(inputBuffer);
   const buffer = yield uncompress(compressed, { asBuffer: true });
-  t.same(buffer, inputBuffer);
+  t.deepEqual(buffer, inputBuffer);
 });
 
 test('uncompress() returning a String', function * (t) {
   const compressed = yield compress(inputBuffer);
   const string = yield uncompress(compressed, { asBuffer: false });
-  t.same(string, inputString);
+  t.deepEqual(string, inputString);
 });
 
 test('uncompress() does not change opts', function * (t) {
   const compressed = yield compress(inputBuffer);
   const opts = {};
   yield uncompress(compressed, opts);
-  t.same(opts, {});
+  t.deepEqual(opts, {});
 });
 
 test('uncompress() on bad input', function * (t) {
@@ -93,26 +93,26 @@ test('uncompress() on not a Buffer', function * (t) {
 test('uncompressSync() defaults to Buffer', function * (t) {
   const compressed = yield compress(inputBuffer);
   const buffer = uncompressSync(compressed);
-  t.same(buffer, inputBuffer);
+  t.deepEqual(buffer, inputBuffer);
 });
 
 test('uncompressSync() returning a Buffer', function * (t) {
   const compressed = yield compress(inputBuffer);
   const buffer = uncompressSync(compressed, { asBuffer: true });
-  t.same(buffer, inputBuffer);
+  t.deepEqual(buffer, inputBuffer);
 });
 
 test('uncompressSync() returning a String', function * (t) {
   const compressed = yield compress(inputBuffer);
   const string = uncompressSync(compressed, { asBuffer: false });
-  t.same(string, inputString);
+  t.deepEqual(string, inputString);
 });
 
 test('uncompress() does not change opts', function * (t) {
   const compressed = yield compress(inputBuffer);
   const opts = {};
   uncompressSync(compressed, opts);
-  t.same(opts, {});
+  t.deepEqual(opts, {});
 });
 
 test('uncompressSync() on bad input', function * (t) {
