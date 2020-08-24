44108cd49ade112199178fea59ef8d1dff0fadfd
Thomas Reggi <thomas.reggi@gmail.com>
2020-08-24 18:37:10 -0400

    fix: add uncompress overload without options

--- snappy.d.ts
+++ snappy.d.ts
@@ -2,5 +2,6 @@ export function compress(input: string | Buffer, callback: (err: Error, buffer?:
 export function compressSync(input: string | Buffer): Buffer;
 export var isValidCompressed: (buffer: Buffer, callback: (err: Error | null, isValid?: boolean) => void) => void;
 export var isValidCompressedSync: (buffer: Buffer) => boolean;
+export function uncompress(buffer: Buffer, callback: (err: Error, uncompressed?: string | Buffer) => void): void;
 export function uncompress(compressed: Buffer, opts: any, callback: (err: Error, uncompressed?: string | Buffer) => void): void;
 export function uncompressSync(compressed: Buffer, opts: any): string | Buffer;
